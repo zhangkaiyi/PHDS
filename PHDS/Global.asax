19f567d9ab94b8e5d8ea6b078f7ba6a357353576
zhangkaiyi <116307766@qq.com>
2016-04-10 16:55:52 +0800

    adding mvc project

---
+++ PHDS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PHDS.MvcApplication" Language="C#" %>
---
+++ PHDS/Models/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PHDS.Models
+{
+    public class HomeModel
+    {
+        [Display(Name = "电子邮件")]
+        public string Name { get; set; }
+    }
+}
