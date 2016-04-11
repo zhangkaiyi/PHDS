787793b6a214f62920845ba3b384bfd0cbd2d710
zhangkaiyi <116307766@qq.com>
2016-04-11 15:26:24 +0800

    update

--- PHDS/Controllers/SalesController.cs
+++ PHDS/Controllers/SalesController.cs
@@ -17,10 +17,16 @@ namespace PHDS.Controllers
             using (var pinhua = new PinhuaEntities())
             {
                 var set = from p in pinhua.发货
-                          select new Models.SalesModels.OrdersModel {
+                          select new Models.SalesModels.OrdersModel
+                          {
+                              RCID = p.ExcelServerRCID,
                               OrderId = p.送货单号,
                               CustomerId = p.客户编号,
-                              CustomerName = p.客户
+                              CustomerName = p.客户,
+                              SaleDate = p.送货日期.Value,
+                              CustomerAddress = p.地址,
+                              SalesTypeId = p.业务类型,
+                              SalesTypeDescription = p.业务描述,
                           };
 
                 return View(set.ToList());
--- PHDS/Models/SalesModels.cs
+++ PHDS/Models/SalesModels.cs
@@ -11,14 +11,30 @@ namespace PHDS.Models
     {
         public class OrdersModel
         {
+            [Display(Name = "唯一标识")]
+            public string RCID { get; set; }
+
             [Display(Name = "订单编号")]
             public string OrderId { get; set; }
+
+            [Display(Name = "销售日期")]
+            public DateTime SaleDate { get; set; }
 
             [Display(Name = "客户编号")]
             public string CustomerId { get; set; }
 
             [Display(Name = "客户名称")]
             public string CustomerName { get; set; }
+
+            [Display(Name = "客户地址")]
+            public string CustomerAddress { get; set; }
+
+            [Display(Name = "销售类型")]
+            public string SalesTypeId { get; set; }
+
+            [Display(Name = "销售类型描述")]
+            public string SalesTypeDescription { get; set; }
+
         }
     }
 }
