67904ca67dd19b336be1aaea13b4f771d356a612
zhangkaiyi <116307766@qq.com>
2016-04-18 01:49:41 +0800

    update

--- PHDS/Models/SalesModels.cs
+++ PHDS/Models/SalesModels.cs
@@ -53,7 +53,7 @@ namespace PHDS.Models
 
             [Required]
             [Display(Name = "名称")]
-            public string 描述 { get; set; }
+            public string Description { get; set; }
 
             [Required]
             [Display(Name = "规格")]
@@ -64,18 +64,23 @@ namespace PHDS.Models
             public int PCS { get; set; }
 
             [Required]
-            public decimal 单位数量 { get; set; }
+            [Display(Name = "单位数量")]
+            public decimal UnitQuantity { get; set; }
 
-            [Required]
-            public string 计价单位 { get; set; }
+            [Display(Name = "计价单位")]
+            public string ChargeUnit { get; set; }
+            
+            [Display(Name = "单价")]
+            public decimal Price { get; set; }
 
-            public decimal 单价 { get; set; }
+            [Display(Name = "金额")]
+            public decimal Amount { get; set; }
 
-            public decimal 金额 { get; set; }
+            [Display(Name = "木种")]
+            public string WoodSpecies { get; set; }
 
-            public string 木种 { get; set; }
-
-            public string 工艺 { get; set; }
+            [Display(Name = "工艺")]
+            public string Processing { get; set; }
 
             public string RCID { get; set; }
         }
